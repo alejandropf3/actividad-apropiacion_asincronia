c02b3867a9aea373d7664fc30ad2f36865064df0
alejandropf3 <alejandropefa31@gmail.com>
2025-12-15 08:19:22 -0500

    Creo el archivo punto2.js y realizo el punto 2 de la guia

---
+++ punto2.js
@@ -0,0 +1,8 @@
+
+console.log("Se inicia el ciclo de repeticion, espera que termine para ver un mensaje") 
+
+for(let i = 0; 1 < 100000000000; i++){ //Ciclo de repeticion muy largo que bloquea la siguiente accion
+    
+}
+
+console.log("Este es el mensaje de finalizacion :)") //Mensaje en caso de que termine el ciclo
